57f9c718de2730d1d0161a35eb49e3895ff622a2
Fuadi <73020820+fdy7n@users.noreply.github.com>
2022-01-06 21:08:48 +0700

    Nickname

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Nick", "Cewe", "Akan", "Di", "Random"],
+  nicknames: ["Honey", "Cewe q", "Akan", "c", "Random"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
